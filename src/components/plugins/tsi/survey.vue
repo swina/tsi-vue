33aee912017044390faa515343a8130d17f7a061
Swina Allen <swina.allen@gmail.com>
2021-04-16 12:24:34 +0300

    Fixed front workflow

--- src/components/plugins/tsi/survey.vue
+++ src/components/plugins/tsi/survey.vue
@@ -18,7 +18,7 @@ export default {
     name: 'TSISurvey',
     components: { TsiPerson , TsiSurveyModule },
     data:()=>({
-        status: 'intro'
+        status: 'person'
     }),
     computed:{
         survey(){
